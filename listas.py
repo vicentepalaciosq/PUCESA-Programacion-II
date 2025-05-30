9aa41d9db9b747c25dba6c566067caa0b61903a7
vicentepalaciosq <cheche284palacios@gmail.com>
2025-05-30 09:17:23 -0500

    UPDATE

--- listas.py
+++ listas.py
@@ -1,3 +1,7 @@
+#Listas=[]
+#Diccionarios={}
+# import array
+#Array es una lista de elementos del mismo tipo, se declaran con el tipo de dato y el tamaño. 
 #Remove. Elimina el primer elemento de la lista que coincide con el valor dado.
 #Pop. Elimina el elemento en la posición dada y lo devuelve.
 lista=[10,20,30,30,40,50,30,20]
